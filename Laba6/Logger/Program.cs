2402e873dce2b04b64ccd3fedb7e7eba78c92c78
Dima Vaneka <dimavaneka@gmail.com>
2021-12-16 17:29:48 +0300

    UP
    UP

---
+++ Laba6/Logger/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "d:\\LogBuffer\\Logger.txt";
+
+            using(LogBuffer logger = new LogBuffer(10, 10000, filePath))
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    logger.Add(new Message(Message.MessageType.Error, $"Message -> {i}"));
+                    Thread.Sleep(200);
+                }
+            }
+        }
+    }
+}
